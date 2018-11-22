646f6d5af86feb833be8823c6114b28101de619a
dukon <smaksim@bk.ru>
2018-11-22 11:58:02 +0600

    New test

---
+++ src/com/itvdn/javaStarter/C04_MathPow.java
@@ -0,0 +1,16 @@
+package com.itvdn.javaStarter;
+
+import static java.lang.Math.pow;
+
+public class C04_MathPow {
+
+    public static void main (String[] args){
+        double x = 2, y = 8;
+
+        double result = pow(x, y);
+
+        System.out.println(result);
+
+    }
+
+}
---
+++ src/com/itvdn/javaStarter/C05_MathSqrt.java
@@ -0,0 +1,16 @@
+package com.itvdn.javaStarter;
+
+//import static java.lang.Math.pow;
+
+public class C05_MathSqrt {
+
+    public static void main (String[] args){
+        double x = 256;
+
+        double result = Math.sqrt(x);
+
+        System.out.println("Квадратный корень: "+ result);
+
+    }
+
+}
