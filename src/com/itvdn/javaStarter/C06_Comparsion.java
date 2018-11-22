496c010945bf2f2625f33ee5e51bc36b6f82ce87
dukon <smaksim@bk.ru>
2018-11-22 17:41:26 +0600

    Nsw-nsw

---
+++ src/com/itvdn/javaStarter/C06_Comparsion.java
@@ -0,0 +1,31 @@
+package com.itvdn.javaStarter;
+
+//import static java.lang.Math.pow;
+
+public class C06_Comparsion {
+
+    public static void main (String[] args){
+        byte value1 = 0, value2 = 1;
+        boolean result;
+
+        result = value1 < value2;
+        System.out.println(result);
+
+        result = value1 > value2;
+        System.out.println(result);
+
+        result = value1 >= value2;
+        System.out.println(result);
+
+        result = value1 <= value2;
+        System.out.println(result);
+
+        result = value1 == value2;
+        System.out.println(result);
+
+        result = value1 != value2;
+        System.out.println(result);
+
+    }
+
+}
